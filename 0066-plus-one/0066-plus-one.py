f66e3872d3cb0e77a96e628e4205b3a8fc3fad31
Amar Mohite <96058921+amarmohite2001@users.noreply.github.com>
2023-07-27 21:38:55 -0700

    Time: 41 ms (91.29%), Space: 16.3 MB (69.36%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -2,8 +2,7 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         s = [str(i) for i in digits]
         res = int("".join(s))
-        res +=1
-        temp =str(res)
+        temp =str(res+1)
         return [int(temp[i]) for i in range(len(temp))]
         
         
