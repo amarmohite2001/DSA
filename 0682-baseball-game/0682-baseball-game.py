bea2fb624e37b786843d6971b482e412a8e59bcf
Amar Mohite <96058921+amarmohite2001@users.noreply.github.com>
2023-08-11 13:01:15 -0700

    Time: 42 ms (96.73%), Space: 16.5 MB (43.48%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -1,40 +1,38 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
-#         final = deque()
-#         i=0
-#         while i<len(operations):
-#             if operations[i]=='+':
-#                 a=final[-2]+final[-1]
-#                 final.append(a)
-#             elif operations[i]=='D':
-#                 b = final[-1]*2
-#                 final.append(b)
-#             elif operations[i]=='C':
-#                 final.pop()
-#             else:
-#                 final.append(int(operations[i]))
-#             i+=1
-#         return sum(final)
+        final = deque()
+        for i in operations:
+            if i=="+":
+                a=final[-2]+final[-1]
+                final.append(a)
+            elif i=="D":
+                b = final[-1]*2
+                final.append(b)
+            elif i=="C":
+                final.pop()
+            else:
+                final.append(int(i))
+        return sum(final)
     
-            score_stack = []
+#             score_stack = []
 
-            for o in operations:
+#             for o in operations:
 
 
-                # it is +, D, or C
-                # if stack isn't of sufficient length, then operation is voided
-                if o == "+" and len(score_stack) >= 2:
-                    summed = score_stack[-2] + score_stack[-1]
-                    score_stack.append(summed)
+#                 # it is +, D, or C
+#                 # if stack isn't of sufficient length, then operation is voided
+#                 if o == "+" and len(score_stack) >= 2:
+#                     summed = score_stack[-2] + score_stack[-1]
+#                     score_stack.append(summed)
 
-                elif o == "D" and len(score_stack) >= 1:
-                    doubled = score_stack[-1] * 2
-                    score_stack.append(doubled)
+#                 elif o == "D" and len(score_stack) >= 1:
+#                     doubled = score_stack[-1] * 2
+#                     score_stack.append(doubled)
 
-                elif o == "C" and len(score_stack) >= 1:
-                    score_stack.pop() 
+#                 elif o == "C" and len(score_stack) >= 1:
+#                     score_stack.pop() 
 
-                else: 
-                    score_stack.append(int(o))
+#                 else: 
+#                     score_stack.append(int(o))
 
-            return sum(score_stack)
+#             return sum(score_stack)
