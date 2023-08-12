106970c1a3171028f9e33f0296ec561f92f86121
Amar Mohite <96058921+amarmohite2001@users.noreply.github.com>
2023-08-12 16:15:20 -0700

    Time: 40 ms (81.96%), Space: 16.3 MB (63.72%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,20 +1,29 @@
+# class Solution:
+#     def fib(self, n: int) -> int:
+#         if n == 0:
+#             return 0
+#         if n ==1:
+#             return 1
+#         F1=0
+#         F2=1
+#         i=2
+#         while i<=n:
+#             temp=F1+F2
+#             F1=F2
+#             F2=temp
+#             i+=1
+#         return temp
+            
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n ==1:
-            return 1
-        F1=0
-        F2=1
-        i=2
-        while i<=n:
-            temp=F1+F2
-            F1=F2
-            F2=temp
-            i+=1
-        return temp
-            
-        
+        def fibnum(x,dic1):
+            if x in [0,1]:
+                return x
+            elif x not in dic1:
+                dic1[x]=fibnum(x-1,dic1)+fibnum(x-2,dic1)
+            return dic1[x]
+        mydict={}
+        return fibnum(n,mydict)
     
     
         
