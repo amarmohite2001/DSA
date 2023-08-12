c2ac2e32129c2cba9c49820662962364effefe70
Amar Mohite <96058921+amarmohite2001@users.noreply.github.com>
2023-08-12 16:10:21 -0700

    Time: 36 ms (92.00%), Space: 16.3 MB (26.39%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n ==1:
+            return 1
+        F1=0
+        F2=1
+        i=2
+        last=0
+        while i<=n:
+            temp=F1+F2
+            F1=F2
+            F2=temp
+            last=temp
+            i+=1
+        return last
+            
+        
+    
+    
+        
