26b402aaa87775a02d0471092717cfd550615b2d
Amar Mohite <96058921+amarmohite2001@users.noreply.github.com>
2023-08-13 14:57:06 -0700

    Time: 52 ms (63.39%), Space: 16.8 MB (87.36%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -10,8 +10,8 @@ class Solution:
                 top = mid+1
             else:
                 break
-        # if not top<=bot:
-        #     return False
+        if not top<=bot:
+            return False
         
         # mid = (top+bot)//2
         l,r= 0, COLS-1
