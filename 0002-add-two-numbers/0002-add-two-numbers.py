b2ea4941c3c91a7173432496d509e569d1e61985
Amar Mohite <96058921+amarmohite2001@users.noreply.github.com>
2023-09-18 20:38:25 -0700

    Time: 67 ms (50.65%), Space: 16.4 MB (60.03%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -12,7 +12,9 @@ class Solution:
             l1_val = l1.val if l1 else 0
             l2_val = l2.val if l2 else 0
             total = l1_val + l2_val +carry
+            #modulus operator(taking the reminder if there is any) 12%10 = 2
             head.next = ListNode(total%10)
+            #floor division- takes e.g= 12//10 = 1
             carry = total//10
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
