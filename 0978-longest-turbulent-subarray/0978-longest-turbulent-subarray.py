25fd565701bff9946d63e7b01cbe0ea90b069e3c
Amar Mohite <96058921+amarmohite2001@users.noreply.github.com>
2023-09-29 13:51:54 -0700

    Time: 417 ms (48.51%), Space: 20.7 MB (79.90%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
@@ -4,17 +4,17 @@ class Solution:
         #>,=,< = 1, 0 ,-1
         res, prev = 1, ""
         while r<len(arr):
-            if arr[r-1]>arr[r] and prev !=">":
+            if arr[r-1]>arr[r] and prev !=1:
                 res = max(res, r-l+1)
                 r+=1
-                prev=">"
-            elif arr[r-1]<arr[r] and prev != "<":
+                prev=1
+            elif arr[r-1]<arr[r] and prev != -1:
                 res = max(res, r-l+1)
                 r+=1
-                prev="<"
+                prev=-1
             else:
                 r = r+1 if arr[r]==arr[r-1] else r
                 l=r-1
-                prev=""
+                prev=0
         return res
         
