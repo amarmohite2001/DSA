824c4c9a6078cbeff66d43bb2c784e897afb84cd
Amar Mohite <96058921+amarmohite2001@users.noreply.github.com>
2024-06-12 07:28:45 -0600

    Time: 51 ms (80.18%), Space: 16.6 MB (62.68%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.py
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -5,5 +5,5 @@ class Solution:
             local,domain=e.split("@")
             local=local.split("+")[0]
             local= local.replace(".","")
-            unique.add((local,domain))
+            unique.add(local+'@'+domain)
         return len(unique)
